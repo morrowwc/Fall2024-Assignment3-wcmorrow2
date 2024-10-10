21bb919046bf6f6698f99dbab5e1d4985c0cf8a1
Will Morrow <65681991+morrowwc@users.noreply.github.com>
2024-10-10 12:24:17 -0500

    project setup

---
+++ Fall2024-Assignment3-wcmorrow2/Fall2024-Assignment3-wcmorrow2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment3_wcmorrow2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
