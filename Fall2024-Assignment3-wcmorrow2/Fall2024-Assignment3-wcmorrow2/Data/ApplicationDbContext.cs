c498d116e0b92452483931f4e0681914c4df4c2f
Will Morrow <65681991+morrowwc@users.noreply.github.com>
2024-10-14 12:05:38 -0500

    initialized Movie class

--- Fall2024-Assignment3-wcmorrow2/Fall2024-Assignment3-wcmorrow2/Data/ApplicationDbContext.cs
+++ Fall2024-Assignment3-wcmorrow2/Fall2024-Assignment3-wcmorrow2/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Fall2024_Assignment3_wcmorrow2.Models;
 
-namespace Fall2024_Assignment3_wcmorrow2.Data
+namespace Fall2024_Assignment3_wcmorrow2.Data;
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
+    public DbSet<Fall2024_Assignment3_wcmorrow2.Models.Movie> Movie { get; set; } = default!;
+
 }
---
+++ Fall2024-Assignment3-wcmorrow2/Fall2024-Assignment3-wcmorrow2/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fall2024_Assignment3_wcmorrow2.Models
+{
+    [PrimaryKey(nameof(Id))]
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Year { get; set; }
+    }
+}
